5db56f321367b9671b6b040856ea53e55dd9aa62
Susanne Dewein <susanne.dewein@gmail.com>
2015-01-09 15:37:30 +0100

    remove class method run and use instance methods in main.rb (wip)

--- skript.rb
+++ skript.rb
@@ -1,6 +1,7 @@
 require "pathname"
 require "pry"
 require "awesome_print"
+require_relative 'log_file.rb'
 
 class LogFinder
 
@@ -8,38 +9,47 @@ class LogFinder
     @path = path
   end
 
-  def self.run(path)
-    LogFinder.new(path).find   
-  end
-
-  def find
+  def find(path)
     old_path = Pathname.getwd
     Dir.chdir(@path) #change into folder
 
-    files = Pathname.glob('**/*.log') 
+    pathnames = Pathname.glob('**/*.log') #oder path mit string interpolation übergebn, List of Pathname Objects
 
-    result = sanitized(files)
+    logfiles = convert(pathnames)
 
-    sorted_result = order(result)
+    @sorted_result = order(logfiles)
 
-    Dir.chdir(old_path) #change back into old folder
 
-    ap sorted_result
-    sorted_result
+    Dir.chdir(old_path)
+
+    @sorted_result
+  end
+
+  def delete
+    index = LogFinder.get_input.to_i
+    @path_as_string = (@sorted_result[index])[:name]
+    puts "Are you sure you want to delete #{@path_as_string}? Confirm with Y"
+    answer =  LogFinder.get_input
+    Pathname.new(@path_as_string).delete if answer == 'Y'
+  end
+
+  def self.get_input
+    input = STDIN.gets.chomp # nur gets.chomp (ie Kernel#gets, reads from pathnames given as argument) geht nicht, stattdessen IO#gets (reads from directory name)
+    if input.downcase == "exit"
+      raise "ApplicationWantsToExit"
+    end
+    return input
   end
 
   private
 
-  def sanitized(files)
-    files.map do |file|
-      {
-         name: file.realpath.to_s,
-         size: file.size
-       }
+  def convert(pathnames)
+    pathnames.map do |p|
+      LogFile.new(p)
     end
   end
 
   def order(result)
-    (result.sort_by {|k| k[:size]}).reverse
+    (result.sort_by {|logfile| logfile.size}).reverse
   end
 end
